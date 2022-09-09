12b042e2707b0942d22442787304398eb6fc5104
ugurukku <95143202+ugurukku@users.noreply.github.com>
2022-09-09 21:13:59 +0400

    changes

---
+++ UgurList.java
@@ -0,0 +1,44 @@
+package com.example.webshamaki.fikir;
+
+
+public class UgurList<E> {
+    private Object[] obj = new Object[1];
+
+    public void add(E e) {
+        if (obj[obj.length - 1] == null) {
+
+            obj[obj.length - 1] = e;
+
+        } else {
+            increase();
+            obj[obj.length - 1] = e;
+        }
+    }
+
+    public int size() {
+        return obj.length;
+    }
+
+    public E get(int index) {
+        try{
+        return (E) obj[index];}
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("The index is not available");
+            return null;
+        }
+    }
+
+    private void increase() {
+        Object[] objInstance = new Object[obj.length + 1];
+
+        for (int i = 0; i < obj.length; i++) {
+
+            objInstance[i] = obj[i];
+
+        }
+
+        this.obj = objInstance;
+
+    }
+
+}
